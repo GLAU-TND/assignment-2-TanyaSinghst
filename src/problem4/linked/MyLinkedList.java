06fe75a8db1f43da3292a70d88faf95a26ed936a
TanyaSinghst <tanya.singh_iot18@gla.ac.in>
2020-03-28 18:25:11 +0530

    variable declare and addLast created

--- src/problem4/linked/MyLinkedList.java
+++ src/problem4/linked/MyLinkedList.java
@@ -1,4 +1,18 @@
 package problem4.linked;
 
 public class MyLinkedList {
+    private Node first;
+    private Node last;
+
+    public void addLast(int data) {
+        Node node = new Node(data);
+        if (first == null) {
+            first = last = node;
+            return;
+        }
+        last.setNext(node);
+        last = node;
+    }
+
+
 }
