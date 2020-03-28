6602f1880da5fb7970c5f13553cb04756a6e2de1
TanyaSinghst <tanya.singh_iot18@gla.ac.in>
2020-03-28 16:21:28 +0530

    insert method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,6 +7,7 @@
 package problem5.circularqueue;
 
 import problem5.node.Node;
+import problem5.student.Student;
 
 //to implement circular queue
 public class MyCircularQueue {
@@ -14,6 +15,21 @@ public class MyCircularQueue {
 
     public MyCircularQueue() {
         rear = front = null;
+    }
+
+    public void insert(Student student) {
+        if (student.getAppearing() > 2)
+            System.out.println("CANNOT INSERT THIS STUDENT --> " + student.getName());
+        else {
+            Node n = new Node(student);
+            if (front == null) {
+                front = n;
+            } else
+                rear.setNext(n);
+            rear = n;
+            rear.setNext(front);
+
+        }
     }
 
 }
