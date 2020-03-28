73a6bade4f6d74929969bf793bcf72f482a617e2
TanyaSinghst <tanya.singh_iot18@gla.ac.in>
2020-03-28 15:39:58 +0530

    insert method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -17,4 +17,25 @@ public class MyBinarySearchTree {
     public MyBinarySearchTree() {
         this.root = root;
     }
+
+    public void insert(int data) {
+        TreeNode newnode = new TreeNode(data);
+        if (root == null) {
+            root = newnode;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (newnode.getData() >= temp.getData())
+                    temp = temp.getRight();
+                else
+                    temp = temp.getLeft();
+            }
+            if (newnode.getData() <= parent.getData())
+                parent.setLeft(newnode);
+            else
+                parent.setRight(newnode);
+        }
+    }
 }
